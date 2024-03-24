60883b049da7e8ddaa2d2c43ca48dc9491f83566
Eyad <eyadhajissa@gmail.com>
2024-03-24 14:39:55 -0500

    Updated so that the triangular numbers are used as indicies for the keys rather than the keys themselves. This way the keys need not be sequential for this function to work properly. For exmaple a triangle may now be: 2 5 7 12 22 70
    The indices of the edge of the triangle are 1, 3, 6 and reference 2, 7 and 70 in the list of integers.

--- main.py
+++ main.py
@@ -10,37 +10,37 @@ def decode(msg_file):
 
     print(f'Decoding, {msg_file}')
 
+    #Read the lines of the csv file into a dictionary
     key_value_pairs = {}
-
     for line in lines:
         if len(line.strip()) != 0:
             key, value = line.split(' ')
             key_value_pairs[key.strip()] = value
 
+    #make a list of the keys (number values) and order them from low to high
     nums = list(key_value_pairs.keys())
     nums = [int(x) for x in nums]
     nums.sort()
-    print(key_value_pairs['3'])
-    print(nums)
+    #print(nums)
 
-
-    #make a list of indicies of the edge of a triangle up to the length of the file
+    #find the values that are on the edge a triangle up to the length of the file
     edge = []
     new_edge = 1
     i = 1
     code = ''
-    print('max' + str(max(nums)))
+    #print('max' + str(max(nums)))
+    #print('len' + str(len(nums)))
     while new_edge < int(max(nums)):
         new_edge = i*(i+1)//2
         if new_edge > int(max(nums)):
             raise Exception("Warning: Total number of items does not make a triangle!")
         edge.append(new_edge)
-        code += key_value_pairs[str(new_edge)]
+        #print(new_edge)
+        code += key_value_pairs[str(nums[new_edge-1])]
         i += 1
-        print(new_edge)
 
     code = code.replace('\n', ' ').strip()
-    print(edge)
+    #print(edge)
     print(code)
     return code
 
